ebe6b9adeb0d481f4885a146b79c5f37432ca25f
kabacargah <foriinji@gmail.com>
2023-12-24 01:55:26 +0300

    userdata.sh updated

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "cloudnice"
+}
+
+variable "git-token" {
+  default = "**********************************"
+}
+
+variable "key-name" {
+  default = "keyname"
+}
+
+variable "hosted-zone" {
+  default = "devopsaws.click"
+}
